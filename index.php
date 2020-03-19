2306608fd70cd821fa0744e881aa59174ff6a61e
ElionGreed <56034490+ElionGreed@users.noreply.github.com>
2020-03-19 18:33:38 +0000

    Update index.php

--- index.php
+++ index.php
@@ -111,6 +111,7 @@ if ($resultCheck > 0){
 			<a href="#" class="w3-bar-item w3-button">Link 1</a>
 			<a href="#" class="w3-bar-item w3-button">Link 2</a>
 			<a href="#" class="w3-bar-item w3-button">Link 3</a>
+			<a href="registration.php" class="w3-bar-item w3-button">Link Reg</a>
 		</div>
 
 		<fieldset>
