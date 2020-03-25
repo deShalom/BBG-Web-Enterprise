834855105bced3fb346d36a8b7a6b45c7d86f1bd
pwl97 <54542046+pwl97@users.noreply.github.com>
2020-03-25 00:59:46 +0000

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,16 +1,18 @@
 <?php
 include_once 'config.php';
 
-$sql = "SELECT * FROM accounts";
+$sql = "SELECT * FROM accounts;";
 $result =  mysqli_query($conn, $sql);
-$resultcheck = mysqli_num_rows($results);
+$resultcheck = mysqli_num_rows($result);
 
 if ($resultcheck > 0){
+    while ($row = mysqli_fetch_assoc($result)){ // data imported from db is saved as array called $row
+        echo $row['Username'].'<br>'.$row['pass']; // displays everything under "Username" and "pass"
 
-while ($row = mysqli_fetch_assoc($result)){
-    echo $row['userID'];
-}
+    }
 
 }
 
 ?>
+
+// works
