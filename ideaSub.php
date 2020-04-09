3bbd82b94b975ed46064974541714abd8b6465b6
pwl97 <54542046+pwl97@users.noreply.github.com>
2020-04-09 21:03:54 +0100

    Add files via upload
    Need help grabbing PostID.

--- ideaSub.php
+++ ideaSub.php
@@ -12,85 +12,108 @@ if(!isset($_SESSION['login_user']))
 // opens a connection to the DB via the config file
     include 'config.php';
     
-// Creating a super global variable allowing us to use the data from the html file.
-// Creating a boolean variable to let us know if a post has documents attached. With FALSE as default.
-    $category = mysqli_real_escape_string($conn, $_POST['category']) ;
+// Creating variables allowing us to use the functionality of our other php pages (in this case, minaly ideaSubmission.php)
+    $category1 = mysqli_real_escape_string($conn, $_POST['category1']);
+    $category2 = mysqli_real_escape_string($conn, $_POST['category2']);
+    $category3 = mysqli_real_escape_string($conn, $_POST['category3']);
     $problem = mysqli_real_escape_string($conn, $_POST['problem']);
     $idea = mysqli_real_escape_string($conn, $_POST['idea']);
-    $docupload = FALSE;
+// Creating a boolean variable to let us know if a post has documents attached. With FALSE as default.
+    $docupload = '0';
     $posttitle = mysqli_real_escape_string($conn, $_POST['posttitle']);
     $ideadept = $_POST['department'];
-// Checks if user wants to be anon; if checked return 1, if not return 0.
-    if (isset($_POST['anon'])){
-    $anon = $_POST['anon'] ? 1 : 0;
-    }
-    
-// Grabs the userID from the sessions.
-    $userID = $_SESSION['userID'];
 
-    $file =$_FILES['fileToUpload'];
-   
-// IF File upload statement; gathering the file information, formatting it and sending it to the target directory with a new unique name.
-if (isset($_POST['submitidea']))
+    // if anon checkbox is ticked, it is set to 1; if not, set to 0.
+if( isset($_POST['anon']))
 {
-    if(!isset($_FILES['fileToUpload']) || $_FILES['fileToUpload']['error'] == UPLOAD_ERR_NO_FILE) 
-    {
-        header("Location: index.php?nofileselected"); 
-    } 
-    else 
-    {
-        // Checks if the button "submitidea" has been pressed, "isset" does this for us.
-        $docupload = TRUE;
-        // Variables assinged to super globals allowing us to utilise the file details.
-        
-        // $_Files helps us get all the information from the file that we want to upload. - I also use $file for each other variable to shorten the process
-        $fileName = $file['name'];      // Same shit as above but instead of writing out $_FILES infront of each variable, I set it to $file for conveniences sake.
-        $fileTempName = $file['tmp_name'];
-        $fileSize = $file['size'];
-        $fileError = $file['error']; // this pops up in the file information when uploading it
-        $fileType = $file['type'];
-        
-        // Setting the target directory for the files.
-        $target_dir = ('uploadedDocs/');
-        
-        // This seperates the file name and the file extention (its type)(whatever is before and after the ".")
-        $fileExt = explode('.',$fileName);
-        
-        // This takes the extention of the file, which could be in capital letters, such as JPEG or w/e and makes it all lower case
-        // by grabbing it from the last place in the created array above (via the use of explode).
-        $fileActualExt = strtolower(end($fileExt));
-        
-        // This is an array which sets what file extentions we allow to be uploaded. (add whatever we want to be allowed)
-        $allowed = array('jpg','pdf','png','doc','gif','jpeg','tif');
-             
-        // Checking if uploaded file is allowed by us, file size (10000kbs) check, and unique name changing
-        // and error checking with a nested IF and error msgs.
-        if (in_array($fileActualExt, $allowed))
-        {
-            if ($fileSize < 1000000)
-            {
-                $fileNameNew = uniqid('', true).".".$fileActualExt; // This creates a unique name for each file and adds the extention back (which is now in lower case).
-                $fileDestination = $target_dir.$fileNameNew;
-                move_uploaded_file($fileTempName, $fileDestination); // Function which uploads the file using the temporary space and our final file destination.  
-                header("Location: index.php?UploadSuccess"); // If all goes well, we are take to the Index page with "UploadSuccess" written in the address bar.              
-            }
-            else
-            {
-                echo "The file you are trying to upload is too big!";
-            }               
-        }
-        else 
-        {
-            echo "You are trying to upload a file type we do not support!";
-        }    
-    }    
-}          
+    $anon = "1";
+}
 else
 {
-    header("Location: index.php?nodocuploaded");
-}
-    
+    $anon = "0";
+}    
 
+$userID = $_SESSION['userID']; // Grabs the userID from the sessions.
+
+$file =$_FILES['fileToUpload[]']; // allows us to shortcut setting file variables.
+
+
+if (isset($_POST['submitidea'])){ // If submit button is pressed
+
+    if(isset($_FILES['fileToUpload')){ // checks if filetoupload has been submitted
+        $file_array = $_FILES['fileToUpload']; // create an array to hold the files 
+        for ($i=0; $i < count($file_array); $i++) { // for loop, so we can go through the entire array 1 by 1
+    
+            $allowed = array('jpg','pdf','png','doc','gif','jpeg','tif'); // allowed extentions
+            $target_dir = ('uploadedDocs/'); // target directory
+    
+            $docupload = "1"; // documents have been selected, docupload set to 1 so we can update our db accordingly.               
+            
+            $fileName = $file['name'];           // Using "$file" to save time.
+            $fileTempName = $file['tmp_name'];  // Variables assigned to super globals allowing us to utilise the file details.
+            $fileSize = $file['size'];         // $_Files helps us get all the information from the file that we want to upload. - I also use $file for each other variable to shorten the process
+            $fileError = $file['error'];      // Same shit as above but instead of writing out $_FILES infront of each variable, I set it to $file for conveniences sake, as shown above.
+            $fileType = $file['type'];       // this pops up in the file information when uploading it.
+    
+            $fileExt = explode('.',$fileName); // This seperates the file name and the file extention (its type)(whatever is before and after the ".")
+    
+            $fileActualExt = strtolower(end($fileExt)); // This takes the extention of the file, which could be in capital letters, such as JPEG or w/e and makes it all lower case
+                                                        // by grabbing it from the last place in the created array above (via the use of explode) and setting it to lowercase.
+    
+            if (in_array($fileActualExt, $allowed)) // if actualextention of file is found in the "allowed" array, proceed to upload.
+            {
+                if($fileError === 0){ // checks if file error is 0; if so, moves on.
+
+                    if ($fileSize < 1000000) // checks file size
+                    {
+                        $fileNameNew = uniqid('', true).$userID.".".$fileActualExt; // This creates a unique name for each file and adds the extention back (which is now in lower case).
+                        $fileDestination = $target_dir.$fileNameNew;
+                        move_uploaded_file($fileTempName, $fileDestination); // Function which uploads the file using the temporary space and our final file destination.
+    
+                        $updateDoctable = ("INSERT INTO Documents ('FileType', 'PostID', 'UserID', 'FileName') VALUES ('$fileActualExt', '$postID', '$userID', '$fileNameNew') WHERE $postID = "); //HELP // HELP how to get post id?
+                        mysqli_query($conn, $updateDoctable); // updates Documents table in line with the updated post
+    
+                        header("Location: index.php?DocUploadSuccess"); // If all goes well, we are take to the Index page with "UploadSuccess" written in the address bar.              
+                    }
+                    else
+                    {
+                        echo "The file you are trying to upload is too big!";
+                    } 
+
+                }
+                else // if there was a file error, it will spit out this message.
+                {
+                    echo "There was an error with the upload of your files. Please try again."
+                }
+                              
+            }
+            else 
+            {
+                echo "You are trying to upload a file type we do not support!";
+            } 
+    
+        }
+            // once the loop is done, it will update the Posts table.
+        $updatepostsquery = ("INSERT INTO Posts ('Department', 'Title', 'Body', 'Category1ID', 'Category2ID', 'Category3ID', 'ProblemTxt', 'isUploadedDocuments', 'UserID', 'isAnonymous') 
+                                        VALUES ('$ideadept', '$posttitle', '$problem', '$category1', '$category2', '$category3', '$idea', '$docupload','$userID', '$anon')"); 
+        // updates the Posts table to insert Post information
+        mysqli_query($conn, $updatepostsquery); // executes the query to update Posts table with some Docs being uploaded
+    
+    }
+    else // if button to submit documents has not been pressed
+    {
+        $updatepostsquery = ("INSERT INTO Posts ('Department', 'Title', 'Body', 'Category1ID', 'Category2ID', 'Category3ID', 'ProblemTxt', 'isUploadedDocuments', 'UserID', 'isAnonymous') 
+                                VALUES ('$ideadept', '$posttitle', '$problem', '$category1', '$category2', '$category3', '$idea', '$docupload','$userID', '$anon')"); 
+                                // updates the Posts table to insert Post information
+        mysqli_query($conn, $updatepostsquery); // executes the query to update Posts table with some Docs being uploaded
+        header("Location: index.php?PostSubmittedWithNoDocs"); // takes us back to index if Post has been submitted with no docs uploaded.
+    }
+
+} // if submit button is not pressed; nothing will happen. Don't even need the "else" but hey.
+else
+{
+    
+}
 
 
 
