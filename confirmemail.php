faf0a10edce403c183714b32ab2f8af06db3a385
ElionGreed <56034490+ElionGreed@users.noreply.github.com>
2020-04-09 16:28:26 +0100

    Updated
    Hashed Password works
    Email Confirm works

--- confirmemail.php
+++ confirmemail.php
@@ -1,16 +1,15 @@
 <?php
 	include "config.php";
-
+	
 	if(isset($_GET['email']) || isset($_GET['token']))
 	{
 		$email = mysqli_real_escape_string($conn, $_GET['email']);
 		$token = mysqli_real_escape_string($conn, $_GET['token']);
-
+		
 		$result = mysqli_query($conn, "SELECT Email, token  FROM Accounts WHERE Email='$email' AND token='$token' AND isConfirmed=0");
-		$query = mysqli_fetch_assoc($result)
-		if($query->num_rows > 0)
+		if(mysqli_num_rows($result) > 0)
 		{
-			mysqli_query("UPDATE Accounts SET token='', isConfirmed=0 WHERE Email='$email'");
+			mysqli_query($conn, "UPDATE Accounts SET token='', isConfirmed=1 WHERE Email='$email'");
 			echo "working";
 			//header("location: verified.php");
 		} else
